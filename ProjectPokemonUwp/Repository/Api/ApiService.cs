49dae8bd8226dad652d8a40d7b6e147ca738c6f9
jgersonneto <jgsolucoestecnologicas@gmail.com>
2022-10-06 11:22:40 -0300

    craido classes da api

---
+++ ProjectPokemonUwp/Repository/Api/ApiService.cs
@@ -0,0 +1,64 @@
+﻿using ProjectPokemonUwp.Model;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectPokemonUwp.Repository.Api
+{
+    public class ApiService
+    {
+        public static async Task<Pokemon> ApiPokeById(int id)
+        {
+            try
+            {
+                var idCliente = RestService.For<IApiPoke>("https://pokeapi.co/api/v2");
+                var address = await idCliente.GetPokemonAsyncById(id);
+
+                return address;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro na consulta pokemon: " + e.Message);
+            }
+            return null;
+        }
+
+        public static async Task<Types> ApiPokeByTypes(string type)
+        {
+            try
+            {
+                var idCliente = RestService.For<IApiPoke>("https://pokeapi.co/api/v2");
+
+                var address = await idCliente.GetTypesAsyncByType(type);
+
+                return address;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro na consulta pokemon: " + e.Message);
+            }
+            return null;
+        }
+
+        public static async Task<Pokemon> ApiPokeByName(string name)
+        {
+            try
+            {
+                var idCliente = RestService.For<IApiPoke>("https://pokeapi.co/api/v2");
+
+                var address = await idCliente.GetPokemonAsyncByName(name);
+
+
+                return address;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro na consulta pokemon: " + e.Message);
+            }
+            return null;
+        }
+    }
+}
---
+++ ProjectPokemonUwp/Repository/Api/IApiPoke.cs
@@ -0,0 +1,22 @@
+﻿using ProjectPokemonUwp.Model;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectPokemonUwp.Repository.Api
+{
+    public interface IApiPoke
+    {
+        [Get("/pokemon/{id}")]
+        Task<Pokemon> GetPokemonAsyncById(int id);
+
+        [Get("/pokemon/{name}")]
+        Task<Pokemon> GetPokemonAsyncByName(string name);
+
+        [Get("/type/{name}")]
+        Task<Types> GetTypesAsyncByType(string name);
+    }
+}
